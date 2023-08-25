abd44a01de9fd8f1e7a04890f5d04a1de7c2baa6
marissa.aini <marissa.aini@salt.co.id>
2023-08-25 21:15:38 +0700

    major upgrade

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mrissarissa/go-say-hello
+module github.com/mrissarissa/go-say-hello/v2
 
 go 1.21.0
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello Universe"
+func SayHello(name string) string {
+	return "Hello Universe" + name
 }
